5b4e4d209704d25ffd5931622c668fff305b2a07
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-05-19 01:23:01 -0300

    Uri1004 - Produto Simples

---
+++ Uri1004/Uri1004/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uri1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, prod;
+
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            prod = num1 * num2;
+
+            Console.WriteLine("PROD = " + prod);
+        }
+    }
+}
