07c7ec07a6241a117299206ad59228353698cbf2
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-05-24 00:36:52 -0300

    Uri1011 - Esfera

---
+++ Uri1011/Uri1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double volumeEsfera = (4 / 3.0) * pi * Math.Pow(raio, 3);
+
+            Console.WriteLine("VOLUME = " + volumeEsfera.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
