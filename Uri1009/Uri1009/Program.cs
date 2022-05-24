717bd90407b52aa77d6b676c18cb44bd299ed0a6
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-05-24 00:34:03 -0300

    Uri1009 - Salário com Bônus

---
+++ Uri1009/Uri1009/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeVendedor;
+            double salarioFixo;
+            double vendasTotais;
+            double salarioComissao;
+
+            nomeVendedor = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendasTotais = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salarioComissao = salarioFixo + (15 * vendasTotais / 100);
+
+            Console.WriteLine("TOTAL = R$ " + salarioComissao.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
