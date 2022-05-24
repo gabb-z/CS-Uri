48fbc519fb0e210504fe9da71cf011ba7504aba0
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-05-24 00:32:25 -0300

    Uri1008 - Salário

---
+++ Uri1008/Uri1008/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFuncionario;
+            int horasTrabalhadas;
+            double salarioHora;
+            double salarioTotal;
+
+            numFuncionario = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            salarioHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salarioTotal = salarioHora * horasTrabalhadas;
+
+            Console.WriteLine("NUMBER = " + numFuncionario);
+            Console.WriteLine("SALARY = U$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
