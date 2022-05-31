25ee5ff80aa498ff2c1825c6755c96d00b49a687
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-05-31 01:32:41 -0300

    Uri1013 - O Maior

---
+++ Uri1013/Uri1013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uri1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, maiorAB, maior;
+
+            string[] numeros = Console.ReadLine().Split(' ');
+
+            a = int.Parse(numeros[0]);
+            b = int.Parse(numeros[1]);
+            c = int.Parse(numeros[2]);
+
+            maiorAB = (a + b + Math.Abs(a - b)) / 2;
+            maior = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
