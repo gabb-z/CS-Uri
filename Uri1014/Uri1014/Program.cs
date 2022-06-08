7205dc9027dde7085231441012e37a87806af69d
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-07 22:38:56 -0300

    Uri1014 - Consumo

---
+++ Uri1014/Uri1014/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaKm;
+            double combustivelGasto, kmLitro;
+
+            distanciaKm = int.Parse(Console.ReadLine());
+            combustivelGasto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            kmLitro = distanciaKm * combustivelGasto;
+
+            Console.WriteLine(kmLitro.ToString("F3", CultureInfo.InvariantCulture) + "km/l");
+
+        }
+    }
+}
