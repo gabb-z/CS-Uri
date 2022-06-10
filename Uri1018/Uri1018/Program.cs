5e407369d4662fd05e1760c3ad05a39ed3d3f894
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-09 22:27:34 -0300

    Uri1018 - Cédulas

---
+++ Uri1018/Uri1018/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Uri1018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, contador, resto, nota;
+
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(n);
+
+            resto = n;
+
+            nota = 100;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 50;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 20;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 10;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 5;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 2;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 1;
+            contador = resto / nota;
+            Console.WriteLine(contador + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+        }
+    }
+}
