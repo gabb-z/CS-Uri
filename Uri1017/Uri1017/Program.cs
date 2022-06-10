14baa8afdfe4f73fd79d4f05035df03feaff4eee
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-09 22:26:38 -0300

    Uri1017 - Gasto de Combustível

---
+++ Uri1017/Uri1017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempoViagem, velocidadeMedia, litrosNecessarios;
+
+            tempoViagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            velocidadeMedia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            litrosNecessarios = tempoViagem * velocidadeMedia / 12;
+
+            Console.WriteLine(litrosNecessarios.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
