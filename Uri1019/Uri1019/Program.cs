3cfa4c2950aaba4ba670c182e4c02828991d2f07
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-09 22:28:41 -0300

    Uri1019 - Conversão de Tempo

---
+++ Uri1019/Uri1019/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uri1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, horas, minutos, segundos, resto;
+
+            n = int.Parse(Console.ReadLine());
+
+            horas = n / 3600;
+            resto = n % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
