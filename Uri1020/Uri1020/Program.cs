b5b4987a0ab40ff48c72b55b5483f1c344556ec6
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-09 22:29:33 -0300

    Uri1020 - Idade em Dias

---
+++ Uri1020/Uri1020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeDias, ano, mes, cont, resto;
+
+            idadeDias = int.Parse(Console.ReadLine());
+
+            resto = idadeDias;
+
+            ano = 365;
+            cont = resto / ano;
+            resto = resto % ano;
+
+            Console.WriteLine(cont + " ano(s)");
+
+            mes = 30;
+            cont = resto / mes;
+            resto = resto % mes;
+
+            Console.WriteLine(cont + " mes(es)");
+            Console.WriteLine(resto + " dia(s)");
+        }
+    }
+}
